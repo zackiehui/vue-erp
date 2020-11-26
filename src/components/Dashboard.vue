6433261cd3e6688b4dbb1bc2b0a82d72467df2fb
Zacky Hui <zacky.hui@price.com.hk>
2020-11-26 16:10:37 +0800

    26 NOV

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dashboard">
+    <h1>{{ msg }}</h1>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+        <b-button>Student</b-button>
+        </b-col>
+        <b-col>
+        <b-button variant="danger">Tutor</b-button>
+        </b-col>
+        <b-col>
+        <b-button variant="success">Course</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      msg: 'Welcome to Dashboard in Vue',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import Dashboard from '@/components/Dashboard';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/students',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/tutors',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/courses',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+  ],
+});
